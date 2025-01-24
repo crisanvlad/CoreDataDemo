1be0259264bb1c0737010425bc84ab220b9e8c43
Vlad <crisan.vlad@live.com>
2025-01-24 23:08:17 +0200

    Init Project

---
+++ CoreDataDemo/CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Vlad Crisan on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
